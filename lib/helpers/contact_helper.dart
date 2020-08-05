f74ccca980c085ea1a88585dfc7014eaeede9995
Dayane Rodrigues <dayane.rodrigues@softplan.com.br>
2020-08-05 18:39:14 -0300

    Create Database

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,5 +1,7 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
@@ -7,7 +9,37 @@ final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
 class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
 
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+
+  Future<Database>get db async {
+    if(db != null) {
+      return db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database>initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contacts.db");
+
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
+        await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, "
+              "$nameColumn TEXT, "
+              "$emailColumn TEXT,"
+              "$phoneColumn TEXT,"
+              "$imgColumn TEXT)"
+        );
+    });
+  }
 }
 
 class Contact {
@@ -42,6 +74,6 @@ class Contact {
 
   @override
   String toString() {
-    return "Contato(id: $id, name: $name, phone: $phone, email: $email, img: $img)";
+    return "Contact(id: $id, name: $name, phone: $phone, email: $email, img: $img)";
   }
 }
